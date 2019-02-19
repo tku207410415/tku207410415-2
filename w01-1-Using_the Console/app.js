c0dc2f6433a25db4cb8443c3af8ac8574ea2fc17
tku207410415 <207410415@gms.tku.edu.tw>
2019-02-19 20:12:49 +0800

    w01-1-Using_the Console and w01-2-Variable-let-const

---
+++ w01-1-Using_the Console/app.js
@@ -0,0 +1,28 @@
+console.log('hello world');
+console.log('1222'); //207410415
+console.log(true);
+var greeting='David';//David Liao
+console.log(greeting);
+console.log([1,2,3,4]);//2019-02-19
+console.log({firstname:'David',lastname:'Liao'});
+console.table({firstname:'David',lastname:'Liao'});//OBJECT
+console.error('This is some error');
+console.warn('This is some warning');
+console.time('hello');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.timeEnd('hello');
+
+//console.clear();
+/*
+multi
+line
+comment
+*/
---
+++ w01-2-Variable-let-const/app.js
@@ -0,0 +1,52 @@
+//var,let,const
+var name="David Liao"
+console.log(name);
+name ='廖偉成';
+console.log(name);
+
+name="2019";
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting='Hello';
+console.log(greeting);
+
+//letter,numbers,_,$,
+//cannot start with number
+
+//multi word vars
+var firstName="David";
+var first_name="Liao";
+var FirstName="sara";
+var firstname;
+
+//let
+
+let name2="David Liao";
+console.log(name2);
+name2 ='廖偉成';
+console.log(name2);
+name2="2019";
+console.log(name2);
+
+//const
+// const name3="David Liao";
+// console.log(name3);
+// name3 ='廖偉成';
+// console.log(name3);
+// name3="2019";
+// console.log(name3);
+
+const person={
+    name:'hua',
+    age:19
+
+};
+person.name='huaDavid';
+person.age=20;
+console.log(person);
+
+const numbers =[1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
