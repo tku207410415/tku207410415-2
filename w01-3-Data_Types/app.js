8bb5e09a1b2ce21284b595b266805f95977d1bd5
tku207410415 <207410415@gms.tku.edu.tw>
2019-02-19 20:44:23 +0800

    w01-2-Variable-let-const and w01-3-Data_Types

---
+++ w01-3-Data_Types/app.js
@@ -0,0 +1,37 @@
+//primitive
+
+//string
+const name='David';
+console.log(typeof name);
+
+//Number
+const age=45;
+console.log(typeof age);
+
+//Boolean
+const hasKids=true; //true,false
+console.log(typeof hasKids);
+
+//Null
+const car=null;
+console.log(typeof car);
+
+//undefimed
+let test;
+console.log(typeof test);
+
+//symbol
+const sym=Symbol();
+console.log(typeof sym);
+
+//reference types -- objects
+
+//Array
+const hobbies=['movies','music'];
+console.log(typeof hobbies);
+
+
+//Objects
+const today =new Date();
+console.log(today);
+console.log(typeof today);
