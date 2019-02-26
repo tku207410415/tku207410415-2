58506805b2efa0d79a621197a0e7c5c75d62cc57
tku207410415 <207410415@gms.tku.edu.tw>
2019-02-26 21:08:02 +0800

    w02-7_Arrays_Methods

--- w02-7_Arrays_Methods/app.js
+++ w02-7_Arrays_Methods/app.js
@@ -1,5 +1,5 @@
 // Create some arrays
-const numbers = [43,56,33,23,44,36,5];
+const numbers = [43,56,33,23,44,36555,5];
 const numbers2 = new Array(22,45,33,76,54);
 const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
 const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
@@ -7,31 +7,59 @@ const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 let val;
 
 // Get array length
+val = numbers.length;
+console.log(val);
 
 // Check if is array
+val = Array.isArray(numbers);
+console.log(val);
 
 // Get single value
+val = numbers[5];
+console.log(val);
 
 // Insert into array
+numbers[2] =60;
+console.log(numbers);
 
 // Find index of value
+val = numbers.indexOf(5);
+console.log(val);
 
 // MUTATING ARRAYS
 // Add on to end
+numbers.push(90);
+console.log(numbers);
 
 // Add on to front
+numbers.unshift(80);
+console.log(numbers);
 
 // Take off from end
+numbers.pop();
+console.log(numbers);
 
 // Take off from front
+numbers.shift();
+console.log(numbers);
 
 // Splice values
+numbers.splice(2,2);
+console.log(numbers);
 
 // Reverse
+numbers.reverse();
+console.log(numbers);
 
 // Concatenate array
 
 // Sorting arrays
+val=fruit.sort();
+console.log(val);
 // Use the "compare function"
+numbers.sort(function(a,b){return a-b});
+console.log(numbers);
 
 // // Reverse sort
+numbers.sort(function(a,b){return b-a});
+console.log(numbers);
