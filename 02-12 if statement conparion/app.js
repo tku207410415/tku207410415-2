04d5628f5f89affdd8dfc6e422a7bef57de6b370
tku207410415 <207410415@gms.tku.edu.tw>
2019-02-27 20:07:26 +0800

    if statement conparion

---
+++ 02-12 if statement conparion/app.js
@@ -0,0 +1,9 @@
+const man ='man';
+const man_age ='48';
+const woman ='woman';
+const woman_age ='13';
+
+val = (man_age<=18 ? 'kid':'adult');
+console.log(val);
+val = (woman_age<=18 ? 'kid':'adult');
+console.log(val);
