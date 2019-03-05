22421bd5301c81395b1c1bebc7a0a3b3a9e69492
tku207410415 <207410415@gms.tku.edu.tw>
2019-03-05 20:26:10 +0800

    w03-10-Function_Declarations and w03-12_Loops and w3-11-tku60

--- w03-10-Function_Declarations/app.js
+++ w03-10-Function_Declarations/app.js
@@ -3,13 +3,41 @@ function greet(firstName='John',lastName='Cena')
 {
     return 'Hello ' + firstName + ' ' + lastName;
 }
- console.log(greet());
+ console.log(greet(' '));
  console.log(greet('ccc' , 'aaa'));
 
 // FUNCTION EXPRESIONS
-
+const square = function(x=3){
+    return x*x;
+}
+console.log(square(5));
 
 // IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+    
+
+(function greeting(){
+    console.log('IIFEs');
+})();
 
 
+
+(function greet(firstName='John',lastName='Cena')
+{
+    console.log('Hello ' + firstName + ' ' + lastName);
+})('Dave','Smith');
+ 
 // PROPERTY METHODS
+const todo = {
+    add: function(){
+        console.log('add todo ...');
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`);
+    }
+}
+todo.delete = function(id){
+    console.log(`Delete todo id ${id}`);
+}
+todo.add();
+todo.edit(111);
+todo.delete(11);
