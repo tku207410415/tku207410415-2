d9d29d2a1b3fc76d0453db0b03443e042a185f06
tku207410415 <207410415@gms.tku.edu.tw>
2019-03-05 19:20:25 +0800

    w03-10-Function_Declarations andw3-11-tku60

---
+++ w03-10-Function_Declarations/app.js
@@ -0,0 +1,15 @@
+// FUNCTION DECLARATIONS
+function greet(firstName='John',lastName='Cena')
+{
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+ console.log(greet());
+ console.log(greet('ccc' , 'aaa'));
+
+// FUNCTION EXPRESIONS
+
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+
+// PROPERTY METHODS
