03d36717a9ba742de1887a3dcbfebdb50192f132
tku207410415 <207410415@gms.tku.edu.tw>
2019-03-13 00:44:54 +0800

    w04-dom-3_Creating_Elements and w04-dom-4_Removing_Replacing_Elements

---
+++ w04-dom-4_Removing_Replacing_Elements/app.js
@@ -0,0 +1,46 @@
+// Create Element
+
+// Add id
+const newHeading = document.createElement('h2');
+// New text node
+newHeading.appendChild(document.createTextNode('Task List'));
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+console.log("oldHeading", oldHeading);
+//Parent
+const cardAction = document.querySelector('.card-action');
+// Replace
+cardAction.replaceChild(newHeading, oldHeading);
+
+// REMOVE ELEMENT
+const lis = document.querySelectorAll('li');
+console.log(lis)
+const list = document.querySelector('ul');
+console.log(list)
+
+// Remove list item
+lis[0].remove();
+// Remove child element
+list.removeChild(lis[3]);
+// CLASSES & ATTR
+const firstLi = document.querySelector('li:first-child');
+console.log("firstLi", firstLi)
+const link = firstLi.children[0];
+let val;
+
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+console.log("link", link)
+link.classList.remove('test');
+val = link;
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+link.removeAttribute('title');
+val = link;
+console.log(val);
